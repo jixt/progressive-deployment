e69444ea8b4a1bca8b25f1dfe158335f3259d6e4
Jan-Rixt Van Hoye <jixt@burntide.eu>
2023-09-06 16:54:57 +0200

    Bumped up version nginx base image

--- delete-app.sh
+++ delete-app.sh
@@ -1,3 +1,9 @@
 #! /bin/bash
 
+# Delete app-for-apps
 kubectl delete -f argocd/app-for-apps/progressive-deployment-app.yaml
+
+# Delete apps
+kubectl delete -f argocd/apps/backend.yaml
+kubectl delete -f argocd/apps/frontend.yaml
+kubectl delete -f argocd/apps/database.yaml
